cefd307dcf5404cf27387e4553c9a71b7e6c4bae
bojanm <bojanmaks@gmail.com>
2018-09-29 20:26:53 +0200

    16

--- wordpress/wp-content/themes/fictional/page.php
+++ wordpress/wp-content/themes/fictional/page.php
@@ -4,9 +4,9 @@ while(have_posts()) {
   the_post(); ?>
 
   <div class="page-banner">
-    <div class="page-banner__bg-image" style="background-image: url(<?php echo get_file_theme_uri('/images/ocean.jpg') ?>);"></div>
+    <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>);"></div>
     <div class="page-banner__content container container--narrow">
-      <h1 class="page-banner__title">Our History</h1>
+      <h1 class="page-banner__title"><?php the_title(); ?></h1>
       <div class="page-banner__intro">
         <p>Learn how the school of your dreams got started.</p>
       </div>
@@ -14,10 +14,16 @@ while(have_posts()) {
   </div>
 
   <div class="container container--narrow page-section">
+<?php
+$theParent = wp_get_post_parent_id(get_the_id());
+ if($theParent){ ?>
 
-    <div class="metabox metabox--position-up metabox--with-home-link">
-      <p><a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> <span class="metabox__main">Our History</span></p>
-    </div>
+  <div class="metabox metabox--position-up metabox--with-home-link">
+    <p><a class="metabox__blog-home-link" href="<?php echo get_permalink($theParent); ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title($theParent); ?></a> <span class="metabox__main"><?php the_title(); ?></span></p>
+  </div>
+
+  <?php }; ?>
+
 
     <!-- <div class="page-links">
       <h2 class="page-links__title"><a href="#">About Us</a></h2>
@@ -28,7 +34,7 @@ while(have_posts()) {
     </div> -->
 
     <div class="generic-content">
-      <?php the_content(); ?>
+      <?php the_content();?>
     </div>
 
   </div>
