7120a47110db4f1bff694468acdd176a0f6e46a2
bojanmaks <bojanmaks@gmail.com>
2018-10-11 16:38:45 +0200

    49

--- wordpress/wp-content/themes/fictional/archive-campus.php
+++ wordpress/wp-content/themes/fictional/archive-campus.php
@@ -12,18 +12,19 @@ pageBanner([
 
     <div class="acf-map">
 
-    <?php
+      <?php
 
-    while(have_posts()) {
-      the_post();
-      $mapLocation = get_field('map_location')
-    ?>
+      while(have_posts()) {
+        the_post();
+        $mapLocation = get_field('map_location')
+      ?>
 
-    <div class="marker" data-lat="<?php echo $mapLocation['lat']; ?>" data-lng="<?php echo $mapLocation['lgn']; ?>">
-
+      <div class="marker" data-lat="<?php echo $mapLocation['lat']; ?>" data-lng="<?php echo $mapLocation['lgn']; ?>">
+        <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+        <?php echo $mapLocation['address']; ?>
+      </div>
+      <?php } ?>
     </div>
-    <?php } echo paginate_links(); ?>
-  </div>
 
   </div>
   <?php
