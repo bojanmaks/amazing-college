28f4535087935349217178c971891d3e28d4cb28
bojanmaks <bojanmaks@gmail.com>
2018-10-23 10:13:21 +0200

    65

--- wordpress/wp-content/themes/fictional/inc/search-route.php
+++ wordpress/wp-content/themes/fictional/inc/search-route.php
@@ -9,6 +9,56 @@ function universityRegisterSearch(){
   ]);
 }
 
-function universitySearchResults(){
-  return '';
+function universitySearchResults($data){
+  $mainQuery = new WP_Query([
+    'post_type' => ['post', 'page', 'professor','program', 'campis', 'event'],
+    's' => sanitize_text_field($data['term'])
+  ]);
+
+  $results = [
+    'generalInfo' => [],
+    'professors' => [],
+    'programs' => [],
+    'events' => [],
+    'campuses' => []
+  ];
+  while($mainQuery->have_posts()){
+    $mainQuery->the_post();
+
+    if(get_post_type() == 'post' || get_post_type() == 'page'){
+      array_push($results['generalInfo'], [
+        'title' => get_the_title(),
+        'permalink' => get_the_permalink()
+      ]);
+    }
+
+    if(get_post_type() == 'professor'){
+      array_push($results['professors'], [
+        'title' => get_the_title(),
+        'permalink' => get_the_permalink()
+      ]);
+    }
+
+    if(get_post_type() == 'program'){
+      array_push($results['programs'], [
+        'title' => get_the_title(),
+        'permalink' => get_the_permalink()
+      ]);
+    }
+
+    if(get_post_type() == 'event'){
+      array_push($results['events'], [
+        'title' => get_the_title(),
+        'permalink' => get_the_permalink()
+      ]);
+    }
+
+    if(get_post_type() == 'campus'){
+      array_push($results['campuses'], [
+        'title' => get_the_title(),
+        'permalink' => get_the_permalink()
+      ]);
+    }
+  }
+  return $results;
 }
